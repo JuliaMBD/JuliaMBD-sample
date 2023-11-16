c565ed5e1625c81e8a5648fa94694a0e1abfb5ce
nakayama <b180998@hiroshima-u.ac.jp>
2023-11-16 09:53:48 +0000

    nakayama

---
+++ success/requirements.jl
@@ -0,0 +1,8 @@
+using Pkg
+Pkg.add("Plots")
+Pkg.add("DifferentialEquations")
+Pkg.add("EzXML")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add(url="https://github.com/JuliaReliab/LookupTable.jl.git")
+Pkg.add(url="https://github.com/JuliaMBD/JuliaMBD.jl.git")
